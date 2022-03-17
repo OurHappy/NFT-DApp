92a0f0cff57765d3df6e541281c68c898997d965
Angela Hsi <applehsi51102@gmail.com>
2022-03-17 17:32:42 +0800

    fix eslint error

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './styleToken.css'
+import './styleToken.css';
 
 const Token = () => {
 
@@ -15,54 +15,54 @@ const Token = () => {
 
   /* Functions */
 
-    // to see whether it is a valid token
-    // 之後會改成 token 是否存在的 API，這邊測試是 token「123」存在
-    let tokenValid = (ID) => {
-        if (ID === "123") return true;
-        else return false;
+  // to see whether it is a valid token
+  // 之後會改成 token 是否存在的 API，這邊測試是 token「123」存在
+  let tokenValid = (ID) => {
+    if (ID === "123") return true;
+    else return false;
+  };
+
+  let searchToken = (event) => {
+    if (tokenValid(event.target.value)) {
+      setShowToken(showToken => 1);
+    } else {
+      setShowToken(showToken => 0);
     }
+  };
 
-    let searchToken = (event) => {
-        if(tokenValid(event.target.value)) {
-        setShowToken(showToken => 1);
-        } else {
-        setShowToken(showToken => 0);
-        }
-    }
+  /* Render functions */
+  return (
+    <div className="divClass">
+      <span className="tokenText">Token:</span>
+      <input
+        type="search"
+        placeholder="Token ID"
+        onChange={searchToken}
+        className="tokenSearchClass"
+      />
 
-    /* Render functions */
-    return (
-        <div className="divClass">
-            <span className="tokenText">Token:</span>
-            <input
-                type="search"
-                placeholder="Token ID"
-                onChange={searchToken}
-                className="tokenSearchClass"
-            />
-
-            {/* if token exists, show the token */}
-            <div>
-                {showToken === 1 &&
+      {/* if token exists, show the token */}
+      <div>
+        {showToken === 1 &&
                     <div className="container">
-                        {/* Show token metadata if a valid token is provided */}
-                        <div className="row">
-                            <div className="col">
+                      {/* Show token metadata if a valid token is provided */}
+                      <div className="row">
+                        <div className="col">
                                 token image
-                            </div>
-                            <div className="col tokenInfo">
+                        </div>
+                        <div className="col tokenInfo">
                                 Name:  {name} <br />
                                 Description:  {description} <br /><br />
                                 External Link:  {exLink} <br />
                                 Other meta:  {meta} <br /><br />
                                 Total Supply:  {totalSupply} <br />
                                 You owned:  {own}
-                            </div>
                         </div>
+                      </div>
                     </div>}
-            </div>
-          </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Token;
