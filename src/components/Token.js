9a7124265ccb3e94a4f067320800edaa6789ebc7
Jerry Lin <jerry954310@gmail.com>
2022-05-25 22:50:07 +0800

    fix: base64 link

--- src/components/Token.js
+++ src/components/Token.js
@@ -106,6 +106,14 @@ const Token = (props) => {
             "https://api.ipfsbrowser.com/ipfs/get.php?hash="
           );
           window.open(newUri);
+        } else if (uri.includes("base64")) {
+          var string = uri;
+          var iframe =
+            "<iframe width='100%' height='100%' src='" + string + "'></iframe>";
+          var x = window.open();
+          x.document.open();
+          x.document.write(iframe);
+          x.document.close();
         } else {
           window.open(uri);
         }
